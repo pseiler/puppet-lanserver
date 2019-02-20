90ae411127ee6f7c684e46c5f7969fac546b6a49
Philipp Seiler <p.seiler@linuxmail.org>
2019-02-20 18:47:01 +0100

    enhanced lanserver control script new features:  * colors when calling status  * start and stop is now implemented

--- lanserver/files/lanserver.sh
+++ lanserver/files/lanserver.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
+ERROR=0
 
-SERVICES=("opentracker-ipv4", "transmission-daemon", "dnsmasq", "httpd", "vsftpd", "ngircd" "inotify2announce");
+SERVICES=("opentracker-ipv4" "transmission-daemon" "dnsmasq" "httpd" "vsftpd" "ngircd" "inotify2announce");
+
+check_root() {
+if [ $UID -ne 0 ];
+then        
+    echo "This script needs to be run as root";
+    exit 1;
+fi
+}
 
 case "${1}" in
     start)
+        check_root
         echo "Starting services:";
+        systemctl start ${SERVICES[@]};
+        echo "${SERVICES[@]}";
         ;;
     stop)
+        check_root
         echo "Stopping services:";
+        echo "${SERVICES[@]}";
+        systemctl stop ${SERVICES[@]};
         ;;
     status)
         echo "Service Status:";
-        systemctl status ${SERVICES[@]};
+        for i in ${SERVICES[@]}
+        do
+            if systemctl is-active "${i}" > /dev/null;
+            then
+                echo -e "\"${i}\" is \e[32mrunning\e[39m.";
+            else
+                echo -e "\"${i}\" is \e[31mnot running\e[39m."
+                ERROR=1
+            fi;
+        done;
         ;;
     *)
         echo "Not a supported command. The following are supported:";
@@ -19,3 +43,7 @@ case "${1}" in
         ;;
 esac
 
+if [ $ERROR -ne 0 ];
+then
+    exit 1;
+fi;
